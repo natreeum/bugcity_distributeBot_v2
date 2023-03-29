54c649846e8bd43176c97d7f3a469f7bd553aac5
natreeum <projectkmscom@gmail.com>
2023-03-29 15:41:10 +0900

    feat : exception handling

--- functions/remove.js
+++ functions/remove.js
@@ -1,7 +1,20 @@
 const delB = require('../functions/prismaScripts/delBusiness');
+const getB = require('../functions/prismaScripts/getBusiness');
 
 module.exports = async function remove(interaction) {
   const bName = interaction.options.getString('사업체이름');
+  const bsns = await getB(bName);
+  if (!bsns)
+    return interaction.reply({
+      content: `해당 사업체가 존재하지 않습니다. 사업체 이름을 확인해주세요.`,
+      ephemeral: true,
+    });
+  if (bsns.activated)
+    return interaction.reply({
+      content: `해당 사업체가 활성화 상태입니다.\n\`/근벅단 비활성화\`명령어를 통해 사업체 비활성화 후 재시도하세요.`,
+      ephemeral: true,
+    });
+
   const delRes = await delB(bName);
   await interaction.reply(`\`${delRes.name}\` 사업체가 삭제되었습니다!`);
 };
