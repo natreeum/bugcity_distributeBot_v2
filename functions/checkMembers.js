1b15e941bd79d51b27e66cae4fb65dd2026bf853
natreeum <projectkmscom@gmail.com>
2023-03-29 12:47:00 +0900

    feat : checkMember with weekly wage

--- functions/checkMembers.js
+++ functions/checkMembers.js
@@ -1,5 +1,6 @@
 const getB = require('../functions/prismaScripts/getBusiness');
 const getMs = require('../functions/prismaScripts/getMems');
+const wageTypes = require('../utils/wageType');
 
 module.exports = async function checkMembers(interaction) {
   const bName = interaction.options.getString('사업체이름');
@@ -8,21 +9,29 @@ module.exports = async function checkMembers(interaction) {
   const ceos = mems.filter((e) => e.level === 'c');
   const executives = mems.filter((e) => e.level === 'e');
   const staffs = mems.filter((e) => e.level === 's');
+  let wageType;
+  const staffsCount = executives.length + staffs.length;
+  if (staffsCount >= 4) wageType = 'type3';
+  else if (staffsCount < 4 && staffsCount > 0) wageType = 'type2';
+  else wageType = 'type1';
+
   let message = `\`${bName}\`의 직원 명단입니다.\n`;
+  if (!business.activated)
+    message += `\n**비활성화된 사업체입니다.\n만약 운영중인 사업체인 경우 관리자에게 문의하세요.**\n`;
   if (ceos.length != 0) {
-    message += `\n사장\n`;
+    message += `\n사장 : ${wageTypes[wageType].c * 7} **BTC**\n`;
     for (const i of ceos) {
       message += `<@${i.discordId}>\n`;
     }
   }
   if (executives.length != 0) {
-    message += `\n임원\n`;
+    message += `\n임원 : ${wageTypes[wageType].e * 7} **BTC**\n`;
     for (const i of executives) {
       message += `<@${i.discordId}>\n`;
     }
   }
   if (staffs.length != 0) {
-    message += `\n직원\n`;
+    message += `\n직원 : ${wageTypes[wageType].s * 7} **BTC**\n`;
     for (const i of staffs) {
       message += `<@${i.discordId}>\n`;
     }
