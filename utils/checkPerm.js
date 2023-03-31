a12368c204601ac157d9d3cf7a0cd6a974352430
natreeum <projectkmscom@gmail.com>
2023-03-31 20:05:57 +0900

    fix : check permission edit

--- utils/checkPerm.js
+++ utils/checkPerm.js
@@ -5,7 +5,14 @@ const GBD_STAFF = [];
 function checkAdmin(uId) {
   return ADMIN.includes(uId);
 }
-function checkGBD(uId) {
+async function checkGBDExist() {
+  const GBD = await getMems('근로벅지공단');
+  if (GBD.length === 0) return false;
+  else return GBD;
+}
+async function checkGBD(uId) {
+  let check = await checkGBDExist();
+  if (!check) check = GBD_STAFF.includes(uId);
   return GBD_STAFF.includes(uId);
 }
 
@@ -15,9 +22,11 @@ async function checkPerm(type, uId, bName) {
   if (type === 'ceo') {
     const mems = await getMems(bName);
     if (mems.length === 0) return 2;
-    const [filteredMem] = mems.filter((e) => e.discordId === uId);
-    if (!filteredMem) return false;
-    return checkAdmin(uId) || checkGBD(uId) || filteredMem.level === 'c';
+    let [filteredMem] = mems.filter((e) => e.discordId === uId);
+    if (!filteredMem) filteredMem = { level: 'uauthorized' };
+    return (
+      checkAdmin(uId) || (await checkGBD(uId)) || filteredMem.level === 'c'
+    );
   }
 }
 
