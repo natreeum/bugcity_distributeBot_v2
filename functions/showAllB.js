5dce293936c077669e9257f868f113c325042900
natreeum <projectkmscom@gmail.com>
2023-03-31 22:50:51 +0900

    fix : check permission edit

--- functions/showAllB.js
+++ functions/showAllB.js
@@ -59,10 +59,11 @@ function addMessage(msgArray, content) {
 }
 
 module.exports = async function showAllB(interaction) {
-  await interaction.reply(`사업체 옆 숫자는 (사장/임원/직원/휴무) 입니다.`);
   // Check Permission
   if (!(await checkPerm('gbd', interaction.user.id)))
     return noPerm(interaction);
+
+  await interaction.reply(`사업체 옆 숫자는 (사장/임원/직원/휴무) 입니다.`);
 
   const businesses = await getBs();
   const activated = businesses.filter((e) => e.activated);
