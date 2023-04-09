1993aea705fd1f9970a1f5927012327c8f4da8d4
natreeum <projectkmscom@gmail.com>
2023-04-10 00:51:11 +0900

    feat : exception handling

--- apiServer/services/getmember.js
+++ apiServer/services/getmember.js
@@ -4,6 +4,8 @@ module.exports = {
   getmember: async (req, res) => {
     const bName = req.params.business;
     const mems = await getMemsByCId(bName);
+    if (!mems)
+      return res.send({ status: 'fail', message: 'No matching business' });
     if (mems.length === 0)
       return res.send({ status: 'fail', message: 'No matching business' });
     else return res.send({ status: 'success', message: mems });
