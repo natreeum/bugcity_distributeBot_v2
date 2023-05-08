ed28cc8a873d866efa8701047d5bf59280efe82f
natreeum <projectkmscom@gmail.com>
2023-05-08 19:30:11 +0900

    test

--- utils/getTotalWage.js
+++ utils/getTotalWage.js
@@ -24,6 +24,7 @@ module.exports = {
       }
       for (const m of mems) {
         const wage = paidWageCheck(paidRes, m.discordId, type[m.level] * 7);
+        console.log(wage);
         total_wage += wage;
       }
     }
