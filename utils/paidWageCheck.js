85dc8e1c0206d6e761e547756e3b2c1cafb5d281
natreeum <projectkmscom@gmail.com>
2023-05-08 19:53:19 +0900

    fff

--- utils/paidWageCheck.js
+++ utils/paidWageCheck.js
@@ -3,6 +3,7 @@
 module.exports = function paidWageCheck(paidRes, uId, wage, maxWage) {
   if (!paidRes[uId]) {
     paidRes[uId] = wage;
+    console.log('type1');
     return wage;
   }
   if (paidRes[uId] + wage > maxWage) {
@@ -10,9 +11,11 @@ module.exports = function paidWageCheck(paidRes, uId, wage, maxWage) {
     let res = 0;
     if (calced >= 0) res = calced;
     paidRes[uId] += res;
+    console.log('type2');
     return res;
   } else {
     paidRes[uId] += wage;
+    console.log('type3');
     return wage;
   }
 };
