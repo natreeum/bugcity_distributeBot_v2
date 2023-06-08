77495c01962a75e18b69b1c6089aa754486e6f71
natreeum <projectkmscom@gmail.com>
2023-06-08 13:14:39 +0900

    max wage 10000

--- utils/paidWageCheck.js
+++ utils/paidWageCheck.js
@@ -1,12 +1,17 @@
-// const { maxWage } = require('../utils/wageVal');
+const { maxWage } = require("../utils/wageVal");
 
-module.exports = function paidWageCheck(paidRes, uId, wage, maxWage) {
+module.exports = function paidWageCheck(
+  paidRes,
+  uId,
+  wage,
+  maxWageParam = maxWage
+) {
   if (!paidRes[uId]) {
     paidRes[uId] = wage;
     return wage;
   }
-  if (paidRes[uId] + wage > maxWage) {
-    const calced = maxWage - paidRes[uId];
+  if (paidRes[uId] + wage > maxWageParam) {
+    const calced = maxWageParam - paidRes[uId];
     let res = 0;
     if (calced >= 0) res = calced;
     paidRes[uId] += res;
